1f3000ea7f2bd8a53feaa4c774445f73707c008e
Kush Rawat <rawatkush0704@gmail.com>
2022-02-19 15:35:43 +0530

    chart added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
 import './Expenses.css';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
   const [selectedYear, setSelectedYear] = useState('2022');
@@ -11,17 +12,19 @@ const Expenses = (props) => {
     setSelectedYear(filter);
   };
 
-  const filteredExepenses = props.expenses.filter((expense) => {
+  const filteredExpenses = props.expenses.filter((expense) => {
     return expense.date.getFullYear().toString() === selectedYear;
   });
+  // console.log(filteredExpenses)
 
   return (
     <Card className='expenses'>
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesFilter
         selected={selectedYear}
         onSaveExpenseFilter={saveExpenseFilterHandler}
       />
-      <ExpensesList items={filteredExepenses}/>
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
